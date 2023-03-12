25eda2ca1e86a7078878193decacf639dc48631f
gacurl <greg.a.curl@gmail.com>
2023-03-12 16:52:45 -0400

    Solve decades; change file name

--- years_to_hours.rb
+++ years_to_hours.rb
@@ -6,3 +6,8 @@ years = gets.chomp # this returns a string
 years = years.to_i # this converts a string to an integer
 hours = years * 365 * 24
 puts "That's #{hours} hours."
+
+puts "Enter a number of decades"
+decades = gets.chomp.to_i
+minutes = decades * 10 * 365 * 24 * 60
+puts "That's #{minutes} minutes" 
